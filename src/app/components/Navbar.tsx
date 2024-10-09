4fd98c2a358c5a939913ce559ae9766cb8a3e2c4
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-10-09 23:40:55 +0500

    fix type any and navbar name issue

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
-export default function (){
+export default function Navbar (){
 
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
